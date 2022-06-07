0e902da4c773d983202aaf97897474f19158cb1d
RuCorrea <rubn_3520@hotmail.com>
2022-06-07 11:49:49 -0500

    cambio a tipo_de_persona tipo boolean

--- database/migrations/2022_06_04_233834_create_proveedores_table.php
+++ database/migrations/2022_06_04_233834_create_proveedores_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('proveedores', function (Blueprint $table) {
             $table->id();
             $table->string('razon_social',200);
-            $table->boolean('persona_moral/fisica')->default(0);
+            $table->boolean('tipo_de_persona')->default(0);
             $table->string('rfc',13);
             $table->string('domicilio',200);
             $table->string('email',100);
